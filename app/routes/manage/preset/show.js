262146315ed6a6829b1d5c9c2a7f5e6ec8990ba1
Piotr Bakalarski <piotrb5e3@gmail.com>
2016-12-18 22:24:31 +0100

    Fix preset list and details

--- app/routes/manage/preset/show.js
+++ app/routes/manage/preset/show.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params) {
+    "use strict";
+    return this.get('store').findRecord('experiment-preset', params.preset_id, {
+      include: 'experiments'
+    });
+  }
 });
