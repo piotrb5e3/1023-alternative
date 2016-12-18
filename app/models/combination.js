7e04063c12a3011d92c6c2aa40798a0c6fb74cd7
Piotr Bakalarski <piotrb5e3@gmail.com>
2016-12-18 11:45:19 +0100

    Session number and progress; combination status

--- app/models/combination.js
+++ app/models/combination.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   session: DS.belongsTo('experiment-session'),
   lightset: DS.attr('number'),
-  events: DS.hasMany('event')
+  events: DS.hasMany('event'),
+  status: DS.attr()
 });
--- app/models/experiment-session.js
+++ app/models/experiment-session.js
@@ -3,5 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   experiment: DS.belongsTo('experiment'),
   status: DS.attr(),
+  number: DS.attr(),
+  progress: DS.attr(),
   combinations: DS.hasMany('combination')
 });
