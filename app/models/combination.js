e6ce342ed4fbe02e3f05a939477beaddc8e6b7d1
Piotr Bakalarski <piotrb5e3@gmail.com>
2016-12-19 00:40:41 +0100

    Support for user name in combination/lightset

--- app/models/combination.js
+++ app/models/combination.js
@@ -4,7 +4,8 @@ export default DS.Model.extend({
   session: DS.belongsTo('experiment-session'),
   lightset: DS.attr('number'),
   events: DS.hasMany('event'),
-  status: DS.attr(),
+  status: DS.attr('string'),
+  user: DS.attr('string'),
   status_str: Ember.computed('status', function () {
     "use strict";
     return {
