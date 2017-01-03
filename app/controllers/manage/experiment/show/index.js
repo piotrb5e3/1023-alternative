d3de061f8e8c4792ea8ef4a7c5fdc378521dd2ff
Piotr Bakalarski <piotrb5e3@gmail.com>
2017-01-03 17:41:23 +0100

    Create sessions from management panel

--- app/controllers/manage/experiment/show/index.js
+++ app/controllers/manage/experiment/show/index.js
@@ -1,6 +1,22 @@
 import Ember from 'ember';
+import EmberValidations from 'ember-validations';
 
-export default Ember.Controller.extend({
+export default Ember.Controller.extend(EmberValidations, {
+  newSessions: Ember.inject.service(),
+  invisibleErrors: Ember.A([" "]),
+  newSessionsCount: null,
+  validations: {
+    'newSessionsCount': {
+      presence: true,
+      numericality: {
+        greaterThanOrEqualTo: 1,
+        allowBlank: true
+      }
+    }
+  },
+  errors: {
+    newSessionsCount: Ember.A()
+  },
   sortedSessions: Ember.computed.sort('model.sessions', (A, B) => {
     "use strict";
     let dA = new Date(A.get('createdon'));
@@ -10,5 +26,21 @@ export default Ember.Controller.extend({
     } else {
       return -1;
     }
-  })
+  }),
+  actions: {
+    newSessionsSubmit() {
+      "use strict";
+      let count = this.get('newSessionsCount');
+      let experimentId = this.get('model.id');
+      this.validate().then(() => {
+        console.log('Validated!');
+        return this.get('newSessions').createNewSessions(experimentId, count)
+          .then(() => {
+            console.log('Connected!');
+            this.set('newSessionsCount', null);
+            return this.get('content').reload();
+          });
+      }).catch((err) => console.log(JSON.stringify(err)));
+    }
+  }
 });
